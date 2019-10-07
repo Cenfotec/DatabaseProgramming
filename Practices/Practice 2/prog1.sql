cb137a1ec3f79122a95fe866540d466210b295ef
Pablo Bonilla <pbonilla29@hotmail.com>
2019-10-07 14:13:17 -0600

    Update prog1 in Practice 2

--- Practices/Practice 2/prog1.sql
+++ Practices/Practice 2/prog1.sql
@@ -1,9 +1,18 @@
 DECLARE
-	name empleados.nombre%TYPE;
-	
+	nombreEmpleado empleados.nombre%TYPE;
+    departamentoEmpleado departamentos.NOMBRE%TYPE;
+    salarioEmpleado empleados.SALARIO%TYPE;
+    codigoPuestoEmpleado empleados.IDpuesto%TYPE;
 
 BEGIN
-	SELECT nombre INTO name FROM (SELECT nombre, rownum FROM (SELECT nombre FROM empleados ORDER BY nombre ASC) WHERE rownum < 11 ORDER BY rownum DESC) WHERE rownum = 1;
-	DBMS_OUTPUT.PUT_LINE(name);
+	SELECT nombre, departamento, salario, puesto
+	INTO nombreEmpleado, departamentoEmpleado, salarioEmpleado, codigoPuestoEmpleado
+	FROM (SELECT nombre, salario, departamento, puesto, rownum FROM (SELECT e.nombre, e.salario, d.nombre AS departamento, e.IDPUESTO as puesto FROM empleados e INNER JOIN departamentos d on e.iddep = d.iddep ORDER BY e.nombre ASC) WHERE rownum < 11 ORDER BY rownum DESC) WHERE rownum = 1;
+	DBMS_OUTPUT.PUT_LINE(
+	    'Nombre: ' || nombreEmpleado ||
+	    ' Departamento: ' || departamentoEmpleado ||
+	    ' Salario: ' || salarioEmpleado ||
+	    ' Puesto: ' || codigoPuestoEmpleado
+	    );
 END;
 /
